c276815e1bf5c16b377cd3e240e52da861f2f1ac
Yoriiis <joris.daniel@gmail.com>
2022-01-15 01:07:18 +0100

    Add logs

--- templates/typescript/src/decs.d.ts
+++ templates/typescript/src/decs.d.ts
@@ -1,4 +1,5 @@
-// TypeScript does not support package.json "exports" fields.
+// TypeScript does not support package.json "exports" fields
+// https://github.com/microsoft/TypeScript/issues/33079
 // costro/jsx is exposed with this field, so there are no TypeScript declaration files for this file.
 // Declare the module to tell TypeScript to ignore the warning.
 declare module 'costro/jsx';
