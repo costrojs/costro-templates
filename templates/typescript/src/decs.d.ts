2d0a41478b83ce12b363fd4ea469b78162fe009d
Yoriiis <joris.daniel@gmail.com>
2022-01-15 22:48:10 +0100

    Add TS declaration for jsx

--- templates/typescript/src/decs.d.ts
+++ templates/typescript/src/decs.d.ts
@@ -2,4 +2,6 @@
 // https://github.com/microsoft/TypeScript/issues/33079
 // costro/jsx is exposed with this field, so there are no TypeScript declaration files for this file.
 // Declare the module to tell TypeScript to ignore the warning.
-declare module 'costro/jsx';
+declare module 'costro/jsx' {
+	export * from 'costro/types/jsx';
+}
