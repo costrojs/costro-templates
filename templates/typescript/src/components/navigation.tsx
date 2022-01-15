345312b911af499bf27116141abbb8bdf1cd24b2
Yoriiis <joris.daniel@gmail.com>
2022-01-15 01:06:09 +0100

    Add logs

--- templates/typescript/src/components/navigation.tsx
+++ templates/typescript/src/components/navigation.tsx
@@ -4,9 +4,9 @@ import { h } from 'costro/jsx';
 export default function Navigation() {
 	return (
 		<div>
-			{/* @ts-ignore */}
+			{/* @ts-ignore TODO: Remove when Costro types are fixed*/}
 			<Link to="/">Home</Link>
-			{/* @ts-ignore */}
+			{/* @ts-ignore TODO: Remove when Costro types are fixed*/}
 			<Link to="/about">About</Link>
 		</div>
 	);
--- templates/typescript/src/decs.d.ts
+++ templates/typescript/src/decs.d.ts
@@ -1,1 +1,4 @@
+// TypeScript does not support package.json "exports" fields.
+// costro/jsx is exposed with this field, so there are no TypeScript declaration files for this file.
+// Declare the module to tell TypeScript to ignore the warning.
 declare module 'costro/jsx';
