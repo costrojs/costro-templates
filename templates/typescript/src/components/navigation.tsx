c6a575cbb9c925b8b1e4062405a8c211ac6adb05
Yoriiis <joris.daniel@gmail.com>
2022-01-21 20:09:35 +0100

    Update costro package

--- templates/typescript/src/components/navigation.tsx
+++ templates/typescript/src/components/navigation.tsx
@@ -5,11 +5,9 @@ export default function Navigation() {
 	return (
 		<ul>
 			<li>
-				{/* @ts-ignore TODO: Remove when Costro types are fixed*/}
 				<Link to="/">Home</Link>
 			</li>
 			<li>
-				{/* @ts-ignore TODO: Remove when Costro types are fixed*/}
 				<Link to="/about">About</Link>
 			</li>
 		</ul>
