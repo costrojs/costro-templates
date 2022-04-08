c103e704b2e2ecdf13cd56bf30216476fbb3ee86
Yoriiis <joris.daniel@gmail.com>
2022-04-08 18:29:23 +0200

    Fix Jest resolver

--- templates/typescript/config/jest.config.js
+++ templates/typescript/config/jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
 	preset: 'ts-jest/presets/js-with-babel',
 	testEnvironment: 'jsdom',
 	testMatch: ['<rootDir>/tests/**/*.test.js'],
-	resolver: './config/jest-resolver.js',
 	moduleDirectories: ['<rootDir>/node_modules'],
 	transform: {
 		'\\.(js|jsx|ts|tsx)$': ['ts-jest', { configFile: './config/babel.config.js' }]
