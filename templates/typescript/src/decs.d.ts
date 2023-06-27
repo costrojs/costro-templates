7d5d47777498a0f5fd3e629f5b5501813ac8d72f
Yoriiis <joris.daniel@gmail.com>
2023-06-27 20:31:47 +0200

    Add JSX automatic runtime on typescript template

--- templates/typescript/src/decs.d.ts
+++ templates/typescript/src/decs.d.ts
@@ -1,7 +1,11 @@
-// TypeScript does not support package.json "exports" fields
-// https://github.com/microsoft/TypeScript/issues/33079
-// costro/jsx is exposed with this field, so there are no TypeScript declaration files for this file.
-// Declare the module to tell TypeScript to ignore the warning.
 declare module 'costro/jsx' {
+	export * from 'costro/types/jsx';
+}
+
+declare module 'costro/jsx-runtime' {
+	export * from 'costro/types/jsx';
+}
+
+declare module 'costro/jsx-dev-runtime' {
 	export * from 'costro/types/jsx';
 }
